e2882e760abbc4d943f4da10f74d83c4acaf9c4a
vandersonmaroni <vandersongmaroni@gmail.com>
2016-06-01 20:06:11 -0300

    Tirando acentuações no teste novo criado

--- test/br/com/projeto/resources/funcionario.feature
+++ test/br/com/projeto/resources/funcionario.feature
@@ -1,12 +1,12 @@
   # language: pt
   @SalarioTest
   Funcionalidade: Modificar o salario do funcionario
-  Dado o sistema de gestão de funcionários iniciará com os seguintes dados
+  Dado o sistema de gestao de funcionarios iniciar com os seguintes dados
   	|id   | usuario           | salario        |
   	|1    | marcos            | 5000           |
   	|2    | felipe            | 4000           |
   	|3    | saulos            | 3000           |
   	|4    | guilhe            | 7000           |
   
-  Quando o chefe aumentar o salário do funcionario com o id '1' por 5%
-  Entao o salário para o empregado  com o id '1' sera mostrado o valor de 5500
+  Quando o chefe aumentar o salario do funcionario com o id '1' por 5%
+  Entao o salario para o empregado  com o id '1' sera mostrado o valor de 5500
--- test/br/com/projeto/teste/salario/SalarioSteps.java
+++ test/br/com/projeto/teste/salario/SalarioSteps.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 import java.util.List;
+
 import javax.inject.Inject;
 
 import cucumber.api.java.pt.Dado;
@@ -18,17 +19,17 @@ public class SalarioSteps {
 	@Inject
 	private Funcionario funcionario;
 	
-	@Dado("^O sistema de gestão de funcionários iniciará com os seguintes data$")
+	@Dado("^O sistema de gestao de funcionarios iniciar com os seguintes data$")
 	public void o_sistema_de_gestao_de_funcionarios_iniciaria_com_os_seguintes_dados(final List<Funcionario> funcionario) throws Throwable { 
 		gerenciadorDeSalario = new GerenciadorDeSalario(funcionario);
 	}
 	
-	@Quando("^O chefe aumentar o salário do funcionário com o id '(\\d+)' por (\\d+)%$")
-	public void o_chefe_aumentar_o_salario_do_funcionário_com_o_id_por(final int id, final int aumentoPorcentual) throws Throwable {
+	@Quando("^O chefe aumentar o salario do funcionario com o id '(\\d+)' por (\\d+)%$")
+	public void o_chefe_aumentar_o_salario_do_funcionario_com_o_id_por(final int id, final int aumentoPorcentual) throws Throwable {
 		gerenciadorDeSalario.aumentaSalario(id, aumentoPorcentual);
 	}
 	
-	@Entao("^O salário para o empregado  com o id (\\d+)'' sera mostrado o valor de (\\d+)$")
+	@Entao("^O salario para o empregado  com o id (\\d+)'' sera mostrado o valor de (\\d+)$")
 	public void o_salario_para_o_empregado_com_o_id_sera_mostrado_o_valor_de(final int id, final float salario) throws Throwable {
 		funcionario = gerenciadorDeSalario.getHolerite(id);
 		assertThat(funcionario.getSalario(), equalTo(salario));
